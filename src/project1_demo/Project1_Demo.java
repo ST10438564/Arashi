6f4810ed0851f87f542607a8d250c34b32ef5755
unknown <st10438564@vcconnect.edu.za>
2024-03-06 12:41:05 +0200

    first commit

---
+++ src/project1_demo/Project1_Demo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1_demo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Project1_Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello,\b\nwelcome to the \b\nworld of programming!!");
+        
+        System.out.println("Hello");
+                
+        System.out.println("welcome to the");
+                
+        System.out.println("world of programming");        
+    }
+    
+}
